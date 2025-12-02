0a99351a65bc769f59086cc6a3bf51348bfac08b
Lewie Donckers <lewie.donckers@nobleo.nl>
2025-12-02 21:48:00 +0100

    day2 part2

---
+++ day2/part2.cpp
@@ -0,0 +1,107 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <print>
+#include <ranges>
+#include <string>
+
+using T = std::uint64_t;
+
+template <typename T>
+struct to {};
+
+template <typename T, typename R>
+auto operator|(R&& range, to<T>) {
+    return std::forward<R>(range) | std::views::transform([](const auto& e) { return T{e}; });
+}
+
+class Id {
+public:
+    Id() = default;
+    explicit Id(T val) : str_{std::to_string(val)}, val_{val} {}
+    explicit Id(std::string_view str) : str_{str}, val_{std::stoull(str_)} {}
+
+    bool isValid() const {
+        const auto str = std::string_view{str_};
+        const auto size = str.size();
+
+        for (auto i : std::views::iota(1uz, (size / 2) + 1)) {
+            auto parts = str | std::views::chunk(i) | to<std::string_view>{} | std::views::pairwise;
+            if (std::ranges::all_of(parts, [](const auto& p) { return std::apply(std::equal_to<>(), p); })) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    Id& operator++() {
+        ++val_;
+        str_ = std::to_string(val_);
+        return *this;
+    }
+
+    Id operator++(int) {
+        const auto prev = *this;
+        operator++();
+        return prev;
+    }
+
+    bool operator==(const Id& rhs) const { return val_ == rhs.val_; }
+
+    Id operator+(T rhs) const { return Id{val_ + rhs}; }
+    T operator-(const Id& rhs) const { return val_ - rhs.val_; }
+
+    T value() const { return val_; }
+
+private:
+    std::string str_{"0"};
+    T val_{0};
+};
+
+T valueIfValid(const Id& i) { return i.isValid() ? 0 : i.value(); };
+
+auto split(char delim) { return std::views::split(delim) | to<std::string_view>{}; }
+
+template <typename R>
+T sum(R&& range) {
+    return std::ranges::fold_left(std::forward<R>(range), T{0}, std::plus<T>{});
+}
+
+template <typename R>
+auto elem(R&& range, int index) {
+    auto result = std::ranges::next(range.begin(), index, range.end());
+    assert(result != range.end());
+    return *result;
+}
+
+T sumRange(std::string_view str) {
+    const auto range = [str]() {
+        auto items = str | split('-') | to<Id>{};
+        const auto from = elem(items, 0);
+        const auto to = elem(items, 1);
+        return std::views::iota(from, to + 1);
+    }();
+
+    return sum(range | std::views::transform(valueIfValid));
+}
+
+T solve(const std::filesystem::path& path) {
+    const auto input = [&path]() {
+        auto stream = std::ifstream{path};
+        auto string = std::string{};
+        std::getline(stream, string);
+        return string;
+    }();
+
+    return sum(input | split(',') | std::views::transform(sumRange));
+}
+
+int main(int argc, const char** argv) {
+    assert(argc >= 2);
+    const auto path = std::filesystem::path{argv[1]};
+    std::println("{}", solve(path));
+}
