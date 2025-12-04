97abff14377b806fe650952d9c9c21bc0338f69d
Lewie Donckers <lewie.donckers@nobleo.nl>
2025-12-04 11:24:36 +0100

    day4 part2

---
+++ day4/part2.cpp
@@ -0,0 +1,118 @@
+#include "shared/shared.hpp"
+
+#include <cassert>
+#include <cstdint>
+#include <filesystem>
+#include <functional>
+#include <print>
+#include <ranges>
+#include <string>
+#include <utility>
+#include <vector>
+
+struct Location {
+    int row{0};
+    int col{0};
+};
+
+struct Offset {
+    int row{0};
+    int col{0};
+};
+
+struct Size {
+    int rows{0};
+    int cols{0};
+};
+
+bool isValid(Location l, Size s) { return (l.col >= 0) && (l.col < s.cols) && (l.row >= 0) && (l.row < s.rows); }
+
+Location operator+(Location l, Offset o) { return Location{.row = l.row + o.row, .col = l.col + o.col}; }
+
+static constexpr auto neighbors =
+    std::array<Offset, 8>{{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}};
+
+auto validNeighbors(Location l, Size s) {
+    return neighbors | std::views::transform([l](auto o) { return l + o; }) |
+           std::views::filter([s](auto l) { return isValid(l, s); });
+}
+
+auto locations(Size s) {
+    return std::views::cartesian_product(std::views::iota(0, s.rows), std::views::iota(0, s.cols)) |
+           std::views::transform([](auto t) { return Location{std::get<0>(t), std::get<1>(t)}; });
+}
+
+bool isRoll(char c) { return c == '@'; }
+
+class Grid {
+public:
+    void append(std::string_view line) {
+        assert(!line.empty());
+        assert(line.find_first_not_of(".@") == std::string::npos);
+        assert(data_.empty() || (line.size() == data_.front().size()));
+
+        data_.emplace_back(line);
+    }
+
+    Size getSize() const {
+        assert(!data_.empty());
+        return Size{.rows = static_cast<int>(data_.size()), .cols = static_cast<int>(data_.front().size())};
+    }
+
+    char element(Location l) const {
+        assert(isValid(l, getSize()));
+
+        return data_[l.row][l.col];
+    }
+
+    void removeRoll(Location l) {
+        assert(isValid(l, getSize()));
+        assert(isRoll(element(l)));
+
+        data_[l.row][l.col] = '.';
+    }
+
+private:
+    std::vector<std::string> data_;
+};
+
+auto parse(const std::filesystem::path& path) {
+    auto result = Grid{};
+
+    for (auto line : yieldLines(path)) {
+        result.append(line);
+    }
+
+    return result;
+}
+
+auto removableElements(const Grid& grid) {
+    const auto element = std::bind_front(&Grid::element, &grid);
+    const auto size = grid.getSize();
+    return locations(size) | std::views::filter([&](Location l) {
+               return isRoll(element(l)) &&
+                      std::ranges::count_if(validNeighbors(l, size) | std::views::transform(element), isRoll) < 4;
+           }) |
+           std::ranges::to<std::vector>();
+}
+
+auto solve(Grid& grid) {
+    auto result = 0;
+    auto toRemove = removableElements(grid);
+
+    while (!toRemove.empty()) {
+        result += toRemove.size();
+        std::ranges::for_each(toRemove, [&grid](auto l) { grid.removeRoll(l); });
+        toRemove = removableElements(grid);
+    }
+
+    return result;
+}
+
+int main(int argc, const char** argv) {
+    assert(argc >= 2);
+    const auto path = std::filesystem::path{argv[1]};
+    auto grid = parse(path);
+
+    std::println("{}", solve(grid));
+}
